d1a9bb6fd446622c1e37a10cb17dabd2a1199a91
Ryan Kavanagh <rak@debian.org>
2012-01-19 07:03:42 -0500

    Only move dBugs to mailbox once read

--- .imapfilter/config.lua
+++ .imapfilter/config.lua
@@ -156,7 +156,8 @@ GMAIL.INBOX:move_messages(GMAIL['Debian.debian-qa'], debianqa)
 debianprivate = GMAIL.INBOX:contain_field('List-ID', 'debian-private.lists.debian.org')
 GMAIL.INBOX:move_messages(GMAIL['Debian.debian-private'], debianprivate)
 
-dbugs = GMAIL.INBOX:match_from('.*@bugs.debian.org')
+dbugs = GMAIL.INBOX:match_from('.*@bugs.debian.org') *
+        GMAIL.INBOX:is_seen()
 GMAIL.INBOX:move_messages(GMAIL['Debian.dBugs'], dbugs)
 
 -- KDE Stuff
