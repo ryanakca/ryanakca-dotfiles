74c85a6ee16567a6365f18429e75718e9ab44410
Ryan Kavanagh <rak@debian.org>
2012-05-09 08:21:29 -0400

    Fixed epsilon-home screenlayout again

--- .screenlayout/epsilon-home.sh
+++ .screenlayout/epsilon-home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output LVDS1 --mode 1024x600 --pos 344x1080 --rotate normal --output VGA1 --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output VGA1 --mode 1920x1080 --pos 0x0 --rotate normal --output LVDS1 --mode 1024x600 --pos 344x1080 --rotate normal
