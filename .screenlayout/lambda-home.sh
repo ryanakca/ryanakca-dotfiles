2102ecbb0ad319231d597fd74e5d81098d3bfdcf
Ryan Kavanagh <rak@debian.org>
2012-07-17 16:43:06 -0400

    Update lambda-home.sh

--- .screenlayout/lambda-home.sh
+++ .screenlayout/lambda-home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output default --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output VGA-1 --mode 1920x1080 --pos 0x0 --rotate normal --output TV-1 --off --output DVI-I-1 --off
