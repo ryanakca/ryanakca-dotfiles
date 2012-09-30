aa075731677b80876233fb1244aa7a0db1e93cdc
Ryan Kavanagh <rak@debian.org>
2012-09-30 11:55:38 +0100

    Programmatically decide on the version of mycmp to use based on offlineimap.__version__

--- .offlineimap.py
+++ .offlineimap.py
@@ -1,8 +1,27 @@
+import offlineimap
 import re
 
 prioritized = ['INBOX', 'QueensU.INBOX']
 
 def mycmp(x, y):
+  if offlineimap.__version__ < '6.4':
+    return mycmp_oldofflineimap(x, y)
+  else:
+    return mycmp_newofflineimap(x, y)
+
+def mycmp_oldofflineimap(x, y):
+  for prefix in prioritized:
+    xsw = x.startswith(prefix)
+    ysw = y.startswith(prefix)
+    if xsw and ysw:
+      return cmp(x, y)
+    elif xsw:
+      return -1
+    elif ysw:
+      return +1
+  return cmp(x, y)
+
+def mycmp_newofflineimap(x, y):
   for prefix in prioritized:
     xsw = x.visiblename.startswith(prefix)
     ysw = y.visiblename.startswith(prefix)
