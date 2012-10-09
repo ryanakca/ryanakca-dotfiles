9f84116f54dad617acd79bda44f6f854f5d31108
Ryan Kavanagh <rak@debian.org>
2012-10-09 23:06:52 +0100

    I misrefactored offlineimap.py to deal with versions

--- .offlineimap.py
+++ .offlineimap.py
@@ -4,24 +4,6 @@ import re
 prioritized = ['INBOX', 'QueensU.INBOX']
 
 def mycmp(x, y):
-  if offlineimap.__version__ < '6.4':
-    return mycmp_oldofflineimap(x, y)
-  else:
-    return mycmp_newofflineimap(x, y)
-
-def mycmp_oldofflineimap(x, y):
-  for prefix in prioritized:
-    xsw = x.startswith(prefix)
-    ysw = y.startswith(prefix)
-    if xsw and ysw:
-      return cmp(x, y)
-    elif xsw:
-      return -1
-    elif ysw:
-      return +1
-  return cmp(x, y)
-
-def mycmp_newofflineimap(x, y):
   for prefix in prioritized:
     if offlineimap.__version__ < '6.4':
       xsw = x.startswith(prefix)
