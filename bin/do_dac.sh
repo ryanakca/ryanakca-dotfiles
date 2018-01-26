d3f68fd8a4a7e15012b58e31a0874fecfb7faa60
Ryan Kavanagh <rak@debian.org>
2018-01-26 14:57:48 -0500

    Mute built-in analog stereo when loading dac

--- bin/do_dac.sh
+++ bin/do_dac.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+pacmd set-sink-mute alsa_output.pci-0000_00_1b.0.analog-stereo 1
 pacmd set-card-profile alsa_card.usb-FiiO_DigiHug_USB_Audio-01 output:iec958-stereo
 pacmd load-module module-ladspa-sink sink_name=binaural sink_master=alsa_output.usb-FiiO_DigiHug_USB_Audio-01.iec958-stereo plugin=bs2b label=bs2b control=700,4.5
