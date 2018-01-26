b055f77f6c91ed0783b3dd5cb5b3c136cca7a921
Ryan Kavanagh <rak@debian.org>
2018-01-26 15:00:20 -0500

    Reload ladspa-sink

--- bin/do_dac.sh
+++ bin/do_dac.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 pacmd set-sink-mute alsa_output.pci-0000_00_1b.0.analog-stereo 1
 pacmd set-card-profile alsa_card.usb-FiiO_DigiHug_USB_Audio-01 output:iec958-stereo
+pacmd unload-module module-ladspa-sink
 pacmd load-module module-ladspa-sink sink_name=binaural sink_master=alsa_output.usb-FiiO_DigiHug_USB_Audio-01.iec958-stereo plugin=bs2b label=bs2b control=700,4.5
