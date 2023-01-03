6f2d8aa231ed8223ca6a951d8ade778fc2da1102
Ryan Kavanagh <rak@rak.ac>
2023-01-03 12:18:15 -0500

    bluetooth bug

--- bin/executable_do_blue.sh
+++ bin/executable_do_blue.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-pacmd load-module module-bluez5-discover
+pacmd list-modules module-bluez5-discover | grep -q module-bluez5-discover || \
+    pacmd load-module module-bluez5-discover
 echo "connect CD:0D:69:69:9A:1B" | bluetoothctl
