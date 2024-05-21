65e1b523c7dbb9d36e01aeafc0ab76df847cc6fe
Ryan Kavanagh <rak@rak.ac>
2024-05-21 11:18:57 -0400

    Update environment variable export script

--- dot_config/sh/01_environment.d
+++ dot_config/sh/01_environment.d
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-for f in ${HOME}/.config/environment.d/*; do
-    . $f
-    for VAR in $(sed -n '/^[^#]/ s/=.*//p' $f); do
-        export $VAR
+for f in "${HOME}/.config/environment.d/"*; do
+    . "$f"
+    sed -n '/^[^#]/ s/=.*//p' "$f" | while read -r VAR; do
+        export "${VAR?}"
     done
 done
